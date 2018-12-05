e26ae217172e4dba94fb5f85d9108429dec46a1d
Nayane <nayane.gomes@aec.com.br>
2018-12-05 18:23:21 -0200

    Trabalho Final

--- Questao3/HelloClient.py
+++ Questao3/HelloClient.py
@@ -1,9 +1,10 @@
 import sys
-import CORBA
+from ansys_corba import CORBA
 
 orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
+print(orb)
+'''''
 helloImpl = orb.string_to_object("Hello")
 helloImpl.sayHello()
 helloImpl.shutdown()
-
-
+'''
